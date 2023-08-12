1bb75c479ba35e9ece8baade8ec142036f3d2807
Sholafunmi <sholafunmiadewara@gmail.com>
2023-08-12 13:56:31 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const showSidebar = document.querySelector(".hamburger");
+const sideMenuBar = document.querySelector(".side-menu-bar");
+const closeSideBar = document.querySelector(".close-menu-bar");
+const sideMenuItems = document.querySelector(".side-menu");
+let bar = [];
+
+
+showSidebar.addEventListener("click", function(){
+sideMenuBar.style.transform = "translate(0%)"
+}) 
+
+// close side bar 
+closeSideBar.addEventListener("click", function(){
+    sideMenuBar.style.transform = "translate(100%)"
+})
+
+sideMenuItems.addEventListener("click", function(){
+    sideMenuBar.style.transform = "translate(100%)"
+})
